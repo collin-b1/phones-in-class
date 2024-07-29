7871c0ffafc58f3fe26cd329543704a7c48ef7d2
collin-b1 <github.1wjd0@simplelogin.com>
2024-07-28 20:58:12 -0400

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://collin-b1.github.io",
-  base: "phones-in-class",
+  base: "/phones-in-class/",
+  output: "static",
 });
